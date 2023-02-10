904b27b4fa7da441150485c4a54650d191b1e87d
monysary <sary.mony@gmail.com>
2023-02-10 13:29:43 -0800

    place fetch request into a form

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,16 +1,49 @@
+import { useState } from 'react'
+
 function Home() {
-  const fetchYelp = async (location, term) => {
-    const response = await fetch(`http://localhost:3000/api/yelp?location=${location}&term=${term}`);
+  const [formState, setFormState] = useState({
+    location: '',
+    term: ''
+  })
 
-    const data = await response.json();
+  const handleInputChange = ({ target: { name, value } }) => {
+    setFormState({ ...formState, [name]: value })
+  }
 
-    console.log(data);
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch(`http://localhost:3000/api/yelp?location=${formState.location}&term=${formState.term}`);
+      
+      const data = await response.json();
+      
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return (
     <div>
       <h1>Home Page</h1>
-      <button onClick={() => fetchYelp('fountain valley ca', 'viet')}>Fetch Yelp</button>
+      <form onSubmit={handleFormSubmit}>
+        <div>Location:</div>
+        <input
+          name='location'
+          placeholder='Location'
+          value={formState.location}
+          onChange={handleInputChange}
+        />
+        <div>Term:</div>
+        <input
+          name='term'
+          placeholder='Keyword'
+          value={formState.term}
+          onChange={handleInputChange}
+        />
+        <button type='submit'>Fetch Yelp</button>
+      </form>
     </div>
   );
 }
