00bc173806c757955cdf981a705bcf32705e1219
monysary <sary.mony@gmail.com>
2023-02-12 23:00:51 -0800

    added more content and styling to dashboard

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,13 @@
 import {
     Grid,
+    Typography,
+    Box
 } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from '../../App.js';
 import dashboardBackground from '../../assets/images/dashboard-background.png';
+
+import auth from '../../utils/auth.js';
 
 import LeftPanel from '../../components/LeftPanel';
 import RightPanel from '../../components/RightPanel';
@@ -13,6 +17,8 @@ function Dashboard() {
     if (!localStorage.getItem('auth_token')) {
         window.location.assign('/');
     }
+
+    const { data: { firstName } } = auth.getProfile();
 
     return (
         <ThemeProvider theme={theme}>
@@ -28,12 +34,35 @@ function Dashboard() {
                         backgroundPosition: 'center'
                     }}
                 >
-                    {/* -----Dashboard----- */}
-                    <Grid container justifyContent='center' wrap='nowrap' sx={{ height: '100vh' }} py='30px' gap='10px'>
-                        <Grid item xs={8}>
+                    <Box height='100vh' overflow='auto'>
+                        <Box p='30px 20% 20px'>
                             <SearchBar />
+                        </Box>
+                        <Grid container wrap='nowrap' justifyContent='space-between' px='40px'>
+                            <Grid item>
+                                <Typography variant='h4' fontFamily='Josefin Sans' color='#F5F5F5'>
+                                    Hello, {firstName}
+                                </Typography>
+                                <Typography variant='subtitle1' fontFamily='Josefin Sans' color='#B2B2B2'>
+                                    Let's make some memories!
+                                </Typography>
+                            </Grid>
+                            <Grid item> 
+                                <Typography variant='h4' fontFamily='Josefin Sans' color='#F5F5F5' textAlign='right'>
+                                    Philippines Trip    
+                                </Typography>
+                                <Typography variant='subtitle1' fontFamily='Josefin Sans' color='#B2B2B2' textAlign='right'>
+                                    Manila, Philippines
+                                </Typography>
+                                <Typography variant='subtitle1' fontFamily='Josefin Sans' color='#B2B2B2' textAlign='right'>
+                                    March 12, 2023 - March 31, 2023
+                                </Typography>
+                            </Grid>
                         </Grid>
-                    </Grid>
+                        <Box p='60px 100px 60px 0px'>
+
+                        </Box>
+                    </Box>
                 </Grid>
                 <Grid item md={2}>
                     <RightPanel />
