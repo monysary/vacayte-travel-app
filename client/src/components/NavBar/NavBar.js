553240ebec9146a5bf661d3937620c0f7534ac55
monysary <sary.mony@gmail.com>
2023-02-27 15:07:59 -0800

    removed search bar and added navbar to homepage instead

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import {
+    Grid,
+} from '@mui/material'
+
+function NavBar({ isDisplayed, setIsDisplayed }) {
+    return (
+        <Grid container>
+
+        </Grid>
+    )
+}
+
+export default NavBar;
--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -13,6 +13,7 @@ import auth from '../../utils/auth.js';
 
 import LeftPanel from '../../components/LeftPanel';
 import SearchBar from '../../components/SearchBar';
+import NavBar from '../../components/NavBar';
 import Welcome from '../../components/Welcome';
 import ViewTrip from '../../components/ViewTrip';
 
@@ -90,7 +91,11 @@ function Dashboard() {
                 >
                     <Box height='100vh' overflow='auto'>
                         <Grid container p='30px 40px 20px' wrap='nowrap' justifyContent='space-between'>
-                            <SearchBar font={font.primary} fontColor={font.color} />
+                            {/* <SearchBar font={font.primary} fontColor={font.color} /> */}
+                            <NavBar
+                                isDisplayed={isDisplayed}
+                                setIsDisplayed={setIsDisplayed}
+                            />
                             <Button variant='outlined' color='secondary' onClick={() => auth.logout()}>
                                 Logout
                             </Button>
