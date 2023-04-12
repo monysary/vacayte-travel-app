9b159a0cd55074857986cc101cd6c0a1296e2f3f
monysary <sary.mony@gmail.com>
2023-04-12 16:59:38 -0700

    pushing to heroku

--- server/controllers/api/yelpApi.js
+++ server/controllers/api/yelpApi.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
     const { location, term } = req.query;
-    const yelpEndpoint = `https://api.yelp.com/v3/businesses/search?limit=10&location=${location}&term=${term}&sort_by=rating`;
+    const yelpEndpoint = `https://api.yelp.com/v3/businesses/search?limit=10&location=${location}&term=${term}&sort_by=rating&cache=false`;
     const yelpAPIKey = process.env.YELP_KEY;
 
     try {
