6295cb3648c5b86ba9a037fc820a1c01f892d0ca
Nicole T <nbtrieu@uci.edu>
2023-04-13 20:50:52 -0700

    initial commit

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGODB_URI || process.env.LOCAL_URI);
+console.log('--------------------------*-------');
+// console.log(process.env.LOCAL_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/vacayteDB',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+);
 
 module.exports = mongoose.connection;
